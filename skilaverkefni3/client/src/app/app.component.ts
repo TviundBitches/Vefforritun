18915ed0f27f8e68f7fa43820f064fbcb3b78892
Johanna Svovudottir <johannasvovudottir@gmail.com>
2017-03-07 16:13:11 +0000

    getsellers

--- skilaverkefni3/client/src/app/app.component.ts
+++ skilaverkefni3/client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import construct = Reflect.construct;
+import { SellersService, Seller } from './sellers.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  private sellers: Seller[];
+
+  constructor(private service: SellersService) {}
+
+  ngOnInit() {
+    this.service.getSellers().subscribe(result => {
+      this.sellers = result;
+    });
+  }
 }
+
---
+++ skilaverkefni3/client/src/app/sellers.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+export interface Seller {
+  id: number;
+  name: string;
+  category: string;
+  imagePath: string;
+}
+
+@Injectable()
+export class SellersService {
+
+  constructor(private http: Http) { }
+
+  getSellers(): Observable<Seller[]> {
+  return this.http.get('http://localhost:5000/api/sellers')
+    .map(response => {
+      return <Seller[]> response.json();
+    });
+  }
+}
