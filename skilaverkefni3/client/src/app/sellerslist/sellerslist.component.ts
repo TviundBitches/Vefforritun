1915b9bc761fd64f8b2c6c3eaf04cce2f8f6f6d9
sigrung15 <sigrung15@ru.is>
2017-03-09 12:52:57 +0000

    modal, not working

--- skilaverkefni3/client/src/app/sellerslist/sellerslist.component.ts
+++ skilaverkefni3/client/src/app/sellerslist/sellerslist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { SellersService, Seller } from '../sellers.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sellerslist',
@@ -11,10 +12,12 @@ import { SellersService, Seller } from '../sellers.service';
 export class SellersListComponent implements OnInit {
   private sellers: Seller[];
   private seller: Seller;
+  closeResult: string;
 
-  constructor(private service: SellersService, private appComponent: AppComponent) {}
+  constructor(private service: SellersService, private appComponent: AppComponent,
+      private modalService: NgbModal) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.service.getSellerById(1).subscribe(result => {
       this.seller = result;
       }, (err) => {
@@ -25,5 +28,23 @@ export class SellersListComponent implements OnInit {
     this.service.getSellers().subscribe(result => {
      this.sellers = result;
      });
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
