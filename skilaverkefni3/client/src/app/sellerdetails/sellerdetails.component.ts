664a935d2c9a40930b358011745fbf74f1fd2352
Unnur Sól <unnuri15@ru.is>
2017-03-09 10:27:29 +0000

    laga error

---
+++ skilaverkefni3/client/src/app/sellerdetails/sellerdetails.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+//import { ToastrService } from 'ngx-toastr';
+import { isNullOrUndefined } from 'util';
+import { isUndefined } from 'util';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './sellerdetails.component.html',
+  styleUrls: ['./sellerdetails.component.css']
+})
+
+export class SellerDetails implements OnInit {
+
+  constructor() {  }
+  ngOnInit() {
+
+    }
+
+}
