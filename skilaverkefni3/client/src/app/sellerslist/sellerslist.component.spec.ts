12dcf0228a20aee0d5fca3574d033beede8683d4
sigrung15 <sigrung15@ru.is>
2017-03-12 11:19:18 +0000

    no errors, app.comp 100%

--- skilaverkefni3/client/src/app/sellerslist/sellerslist.component.spec.ts
+++ skilaverkefni3/client/src/app/sellerslist/sellerslist.component.spec.ts
@@ -46,25 +46,6 @@ describe('AppComponent', () => {
     TestBed.compileComponents();
   });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'Magical Menagerie'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Magical Menagerie');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Magical Menagerie');
-  }));
-
   describe('when sellers service returns empty list of products', () => {
     mockService.successGetProducts = true;
     mockService.productList = [];
