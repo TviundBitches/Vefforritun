cd99f232ab60e71ff63cbdcc453d95da2811466b
sigrung15 <sigrung15@ru.is>
2017-03-13 19:46:37 +0000

    lots of empty tests

--- skilaverkefni3/client/src/app/sellerslist/sellerslist.component.spec.ts
+++ skilaverkefni3/client/src/app/sellerslist/sellerslist.component.spec.ts
@@ -10,11 +10,13 @@ import { AppComponent } from '../app.component';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { Http } from '@angular/http';
+import { ToastrService } from 'ngx-toastr';
 
 describe('SellerslistComponent', () => {
 
   let component: SellerslistComponent;
   let fixture: ComponentFixture<SellerslistComponent>;
+  let seller;
   const mockService = {
     successGetSellers: true,
     sellerList: [{
@@ -42,6 +44,13 @@ describe('SellerslistComponent', () => {
     navigate: jasmine.createSpy('navigate')
   };
 
+  const mockToastr = {
+    error: jasmine.createSpy('error'),
+    success: jasmine.createSpy('success')
+  };
+
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -53,6 +62,9 @@ describe('SellerslistComponent', () => {
       }, {
         provide: NgbModal,
         useValue: mockModal
+      }, {
+        provide: ToastrService,
+        useValue: mockToastr
       }],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       imports: [FormsModule]
@@ -64,14 +76,85 @@ describe('SellerslistComponent', () => {
     fixture = TestBed.createComponent(SellerslistComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  })
+  });
+
+  beforeEach(() => {
+    seller = {
+      onUpdateSeller: function(seller) {
+
+      },
+      addSeller: function() {
+        const modalInstance = this.modalService.open(SellerDlgComponent);
+        modalInstance.componentInstance.seller = {};
+        modalInstance.result.then(obj => {
+          console.log('Dialog was closed using OK');
+          console.log(obj);
+          const params = {
+            id: this.sellers.length + 1,
+            name: obj.name,
+            category: obj.category,
+            imagePath: obj.imagePath
+          }
+          this.service.addSeller(params).subscribe(result => {
+            this.toastrService.success('Þú hefur bætt við notanda!');
+            window.location.reload();
+          })
+        }).catch(err => {
+          console.log('Dialog was cancelled');
+          console.log(err);
+        })
+      }
+    };
+    spyOn(seller, 'addSeller');
+    spyOn(seller, 'onUpdateSeller').and.callThrough();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call addSeller()', async(() => {
-    //expect(component.onUpdateSeller).toHaveBeenCalled();
-    // expect(mockService.getSellers).toBe('Hannyrðaþjónusta Hannesar');
-  }));
+  it('should call addSeller()', function() {
+    expect(seller.addSeller.calls.any()).toEqual(false);
+    seller.addSeller();
+    expect(seller.addSeller.calls.any()).toEqual(true);
+  });
+
+  it('should call onUpdateSeller()', function() {
+    expect(seller.onUpdateSeller).not.toHaveBeenCalled();
+    seller.onUpdateSeller();
+    expect(seller.onUpdateSeller).toHaveBeenCalled();
+  });
+
+  it('should display a list of sellers if the backend returns a list', function() {
+
+  });
+
+  it('should display a message if the list of sellers is empty', function() {
+
+  });
+
+  it('should display an error message if the list cannot be retrieved', function() {
+
+  });
+
+  it('should display a modal dialog if the user tries to add a new seller', function() {
+
+  });
+
+  it('should try to add a new seller if the modal dialog is closed using the OK button', function() {
+
+  });
+
+  it('should NOT try to add a new seller if the modal dialog is closed in any other way', function() {
+
+  });
+
+  it('should add the new seller to the list', function() {
+
+  });
+
+  it('should display an error message if the seller could not be added', function() {
+
+  });
+
 });
