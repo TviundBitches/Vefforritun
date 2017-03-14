53f21f95a9d69b3692a074a07e44bd6a1abe27fe
sigrung15 <sigrung15@ru.is>
2017-03-14 20:25:43 +0000

    90% ;) í sellers.services

--- skilaverkefni3/client/src/app/sellers.service.spec.ts
+++ skilaverkefni3/client/src/app/sellers.service.spec.ts
@@ -2,8 +2,8 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { SellersService } from './sellers.service';
-import { Http, BaseRequestOptions } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+import { Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 
 describe('SellersService', () => {
@@ -39,17 +39,118 @@ describe('SellersService', () => {
 
   it('should getSellers()', () => {
     expect(subject.getSellers()).toBeTruthy();
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({id: 1, name: 'John', category: 'funning'})
+      });
+      connection.mockRespond(new Response(options));
+    });
+    let result;
+
+    subject.getSellers().subscribe(sellers => {
+      expect(sellers).toEqual({id: 1, name: 'John', category: 'funning'});
+    });
   });
 
   it('should getSellerById(1)', () => {
     expect(subject.getSellerById(1)).toBeTruthy();
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({id: 1, name: 'John', category: 'funning'})
+      });
+      connection.mockRespond(new Response(options));
+    });
+    let result;
+
+    subject.getSellerById(1).subscribe(seller => {
+      expect(seller).toEqual({id: 1, name: 'John', category: 'funning'});
+    });
   });
 
   it('should getSellerProducts(1)', () => {
     expect(subject.getSellerProducts(1)).toBeTruthy();
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({id: 1, name: 'vettlingar', price: 22})
+      });
+      connection.mockRespond(new Response(options));
+    });
+    let result;
+
+    subject.getSellerProducts(1).subscribe(products => {
+      expect(products).toEqual({id: 1, name: 'vettlingar', price: 22});
+    });
+  });
+
+  it('should go into updateProduct()', () => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({ success: true})
+      });
+      connection.mockRespond(new Response(options));
+    });
+
+    subject.updateProduct({ id: 1, name: 'vettlingar', price: 22}, 1, 1).subscribe((response) => {
+      expect(response.json()).toEqual({ success: true });
+    })
+  });
+
+  it('should go into updateSeller()', () => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({ success: true})
+      });
+      connection.mockRespond(new Response(options));
+    });
+
+    subject.updateSeller({ id: 1, name: 'Joe', category: 'funning'}, 1).subscribe((response) => {
+      expect(response.json()).toEqual({ success: true });
+    });
+  });
+
+  // it('should getTopSellerProducts(1)', () => {
+  //   backend.connections.subscribe((connection: MockConnection) => {
+  //     let options = new ResponseOptions({
+  //       body: JSON.stringify({id: 1, name: 'vettlingar', price: 22})
+  //     });
+  //     connection.mockRespond(new Response(options));
+  //   });
+  //   let result;
+  //
+  //   subject.getTopSellerProducts(1).subscribe(products => {
+  //
+  //     expect(products).toEqual({id: 1, name: 'vettlingar', price: 22});
+  //   });
+  // });
+
+  it('should go into addProduct()', () => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({ success: true})
+      });
+      connection.mockRespond(new Response(options));
+    });
+
+    subject.addProduct({ id: 1, name: 'vettlingar', price: 22}).subscribe((response) => {
+      expect(response.json()).toEqual({ success: true });
+    });
+  });
+
+  it('should go into addSeller()', () => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      let options = new ResponseOptions({
+        body: JSON.stringify({ success: true})
+      });
+      connection.mockRespond(new Response(options));
+    });
+
+    subject.addSeller({ id: 1, name: 'John', category: 'funning'}).subscribe((response) => {
+      expect(response.json()).toEqual({ success: true });
+    });
   });
 
   // it('should getSellers()', () => {
   //   expect(subject.getSellers()).toBeTruthy();
   // });
+
 });
